00ba3777d9f8cd34a39d49a24178361062167079
JorgeDataware <ji.perea@dataware.com.mx>
2026-01-19 14:03:37 -0600

    Added Result<T> Pattern utilities

---
+++ app/backend/Movies.Module/src/CP.Portal.Movies.Module/Utilities/Abstractions/Result.cs
@@ -0,0 +1,25 @@
+﻿namespace CP.Portal.Movies.Module.Utilities.Abstractions;
+
+public class Result<T>
+{
+    public bool IsSuccess { get; }
+    public Error Error { get; }
+    public T Value { get; }
+
+    protected Result(bool isSuccess, Error error, T value)
+    {
+        IsSuccess = isSuccess;
+        Error = error;
+        Value = value;
+    }
+
+    public static Result<T> Success(T value)
+    {
+        return new Result<T>(true, null, value);
+    }
+
+    public static Result<T> Failure(Error error)
+    {
+        return new Result<T>(false, error, default);
+    }
+}
