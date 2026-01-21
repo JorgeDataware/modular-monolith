259472ba2f2782d52154632c3bf903f6f1aa5f94
JorgeDataware <ji.perea@dataware.com.mx>
2026-01-21 12:54:31 -0600

    Added Genre repository

---
+++ app/backend/Movies.Module/src/CP.Portal.Movies.Module/Domain/Repositories/GenreRepository/GenreRepository.cs
@@ -0,0 +1,20 @@
+﻿
+using CP.Portal.Movies.Module.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace CP.Portal.Movies.Module.Domain.Repositories.GenreRepository;
+
+internal class GenreRepository(MovieDbContext dbContext) : IGenreRepository
+{
+    public void Add(Genre genre)
+        => dbContext.genres.Add(genre);
+
+    public async Task Delete(Guid Id, CancellationToken ct)
+        => await dbContext.genres.Where(m => m.Id == Id).ExecuteDeleteAsync(ct);
+
+    public async Task<IEnumerable<Genre>> GetAllAsync(CancellationToken ct)
+        => await dbContext.genres.ToListAsync(ct);
+
+    public async Task SaveChangesAsync(CancellationToken ct)
+        => await dbContext.SaveChangesAsync(ct);
+}
---
+++ app/backend/Movies.Module/src/CP.Portal.Movies.Module/Domain/Repositories/GenreRepository/IGenreRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace CP.Portal.Movies.Module.Domain.Repositories.GenreRepository;
+
+internal interface IGenreRepository
+{
+    void Add(Genre genre);
+    Task Delete(Guid Id, CancellationToken ct);
+    Task<IEnumerable<Genre>> GetAllAsync(CancellationToken ct);
+    Task SaveChangesAsync(CancellationToken ct);
+}
