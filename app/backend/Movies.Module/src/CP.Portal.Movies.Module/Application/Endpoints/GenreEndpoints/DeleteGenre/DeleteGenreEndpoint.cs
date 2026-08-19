5cf2f28c3d9970fe7b4667ffb9223adac8651653
JorgeDataware <ji.perea@dataware.com.mx>
2026-08-18 22:56:34 -0600

    Added delete endpoint for genre

---
+++ app/backend/Movies.Module/src/CP.Portal.Movies.Module/Application/Endpoints/GenreEndpoints/DeleteGenre/DeleteGenreEndpoint.cs
@@ -0,0 +1,27 @@
+using CP.Portal.Movies.Module.Application.Services.IServices;
+using CP.Portal.Movies.Module.Utilities.Abstractions;
+using CP.Portal.Movies.Module.Utilities.Extensions;
+using FastEndpoints;
+
+namespace CP.Portal.Movies.Module.Application.Endpoints.GenreEndpoints.DeleteGenre;
+
+internal class DeleteGenreEndpoint(IGenreService genreService)
+    : EndpointWithoutRequest<ApiResponse<string>>
+{
+    private readonly IGenreService _genreService = genreService;
+
+    public override void Configure()
+    {
+        Delete("/api/genres/{Id}");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var id = Route<Guid>("Id");
+
+        var result = await _genreService.DeleteGenreAsync(id, ct);
+
+        await this.SendApiResponseAsync(result, "Género eliminado exitosamente", ct: ct);
+    }
+}
