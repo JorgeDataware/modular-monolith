eba859f3bf6c5a6c4800dc5e1116f45426575248
JorgeDataware <ji.perea@dataware.com.mx>
2026-08-18 22:28:56 -0600

    Moved Entities Builder to the file of its class

--- app/backend/Movies.Module/src/CP.Portal.Movies.Module/Domain/Genre.cs
+++ app/backend/Movies.Module/src/CP.Portal.Movies.Module/Domain/Genre.cs
@@ -1,4 +1,7 @@
-﻿namespace CP.Portal.Movies.Module.Domain;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CP.Portal.Movies.Module.Domain;
 
 internal class Genre
 {
@@ -7,4 +10,22 @@ internal class Genre
 
     //Relaciones
     public ICollection<MovieGenre> MovieGenres { get; private set; } = [];
+}
+
+internal class GenreConf : IEntityTypeConfiguration<Genre>
+{
+    public void Configure(EntityTypeBuilder<Genre> builder)
+    {
+        // El primer parámetro es el nombre de la tabla y el segundo es el esquema
+        builder.ToTable("genres", "movies");
+        // Se define la clave primaria Genre.Id
+        builder.HasKey(g => g.Id);
+        // Definir que la base de datos nunca genere los valores para la columna Id
+        builder.Property(g => g.Id).ValueGeneratedNever();
+        // Definir llave foránea con MovieGenre (Uno a Muchos)
+        builder.HasMany(g => g.MovieGenres)
+            .WithOne(mg => mg.Genre)
+            .HasForeignKey(mg => mg.GenreId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
 }
--- app/backend/Movies.Module/src/CP.Portal.Movies.Module/Domain/MovieGenre.cs
+++ app/backend/Movies.Module/src/CP.Portal.Movies.Module/Domain/MovieGenre.cs
@@ -1,4 +1,7 @@
-﻿namespace CP.Portal.Movies.Module.Domain;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CP.Portal.Movies.Module.Domain;
 
 internal class MovieGenre
 {
@@ -7,4 +10,29 @@ internal class MovieGenre
 
     public Movie Movie { get; set; } = null!;
     public Genre Genre { get; set; } = null!;
+}
+
+internal class MovieGenreConf : IEntityTypeConfiguration<MovieGenre>
+{
+    public void Configure(EntityTypeBuilder<MovieGenre> builder)
+    {
+        // El primer parámetro es el nombre de la tabla y el segundo es el esquema
+        builder.ToTable("movie_genres", "movies");
+        // Se define la clave primaria compuesta MovieGenre.MovieId + MovieGenre.GenreId
+        builder.HasKey(mg => new { mg.MovieId, mg.GenreId });
+        // Definir llave foránea con Movie (Muchos a Uno)
+        builder.HasOne(mg => mg.Movie)
+            .WithMany(m => m.MovieGenres)
+            .HasForeignKey(mg => mg.MovieId)
+            .OnDelete(DeleteBehavior.Cascade);
+        // Definir llave foránea con Genre (Muchos a Uno)
+        builder.HasOne(mg => mg.Genre)
+            .WithMany(g => g.MovieGenres)
+            .HasForeignKey(mg => mg.GenreId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // Crear un índice único para acelerar las consultas
+        builder.HasIndex(mg => mg.GenreId);
+        builder.HasIndex(mg => mg.MovieId);
+    }
 }
