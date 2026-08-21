dababd5bbc50189d6c119e02a697ac71ac708a96
Jorge Iván Perea Cárdenas <ji.perea@dataware.com.mx>
2026-08-21 07:44:27 -0600

    Added delete movie endpoint

---
+++ app/backend/Movies.Module/src/CP.Portal.Movies.Module/Application/Endpoints/MovieEndpoints/DeleteMovie/DeleteMovieEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using CP.Portal.Movies.Module.Application.Services.IServices;
+using CP.Portal.Movies.Module.Utilities.Abstractions;
+using CP.Portal.Movies.Module.Utilities.Extensions;
+using FastEndpoints;
+
+namespace CP.Portal.Movies.Module.Application.Endpoints.MovieEndpoints.DeleteMovie;
+
+internal class DeleteMovieEndpoint(IMovieService movieService) : EndpointWithoutRequest<ApiResponse<Guid>>
+{
+    private readonly IMovieService _movieService = movieService;
+
+    public override void Configure()
+    {
+        Delete("api/Movie/{Id}");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync (CancellationToken ct)
+    {
+        var id = Route<Guid>("Id");
+        var result = await _movieService.DeleteMovieAsync(id, ct);
+
+        await this.SendApiResponseAsync(result, "Película eliminada correctamente", ct: ct);
+    }
+}
